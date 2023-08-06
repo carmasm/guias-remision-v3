4eac1122989e376fb7103942347187a8b7902030
User <User@21XDDL3.caracolknits.com>
2023-08-06 01:35:34 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'guias-remision-v3',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
